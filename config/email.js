58e20ae9f85f5df40d3be1928286a3d5bd0e0ab3
utstrabajos2026-design <utstrabajos2026@gmail.com>
2026-05-18 11:08:12 -0600

    Update email.js

--- config/email.js
+++ config/email.js
@@ -11,7 +11,9 @@ const nodemailer = require('nodemailer');
 // ===============================
 
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE || 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
